74311f6de159a63e86b39524aca04a546a55099e
Mew <260057849+mariannew91@users.noreply.github.com>
2026-07-03 20:53:56 +0100

    Revert to API-only FastAPI backend

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from fastapi import Request
-
-app.mount("/resources", StaticFiles(directory="resources"), name="resources")
-
-templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
-allowed_origins = os.getenv("ALLOWED_ORIGINS", "https://mariannew91.github.io/InTown/").split(",")
+allowed_origins = os.getenv(
+    "ALLOWED_ORIGINS",
+    "https://mariannew91.github.io"
+).split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins, 
+    allow_origins=allowed_origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 @app.get("/")
-async def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
+async def root():
+    return {"message": "Hello from the InTown backend!"}
