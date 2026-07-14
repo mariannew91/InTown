81a1ecb48c821828d8ecf6d75b4ae18051ddd4d5
Mew <260057849+mariannew91@users.noreply.github.com>
2026-07-14 12:49:14 +0100

    Added listing JSON

--- main.py
+++ main.py
@@ -16,3 +16,20 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello from the InTown backend!"}
+
+    @app.get("/listings")
+async def get_listings():
+    return [
+        {
+            "id": 1,
+            "title": "Running Club",
+            "type": "Group",
+            "day": "Tuesday"
+        },
+        {
+            "id": 2,
+            "title": "Choir",
+            "type": "Group",
+            "day": "Thursday"
+        }
+    ]
